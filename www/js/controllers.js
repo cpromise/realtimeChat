9b3dace5f31df779eff94c730647804a32b6578d
cpromise <cpromise1@naver.com>
2015-08-15 22:07:36 +0900

    E-mail로 인증하기
    -

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -18,14 +18,30 @@ angular.module('starter.controllers', ['firebase'])
 
 			if($rootScope.authData){
 				//$add는 Firebase에서 제공하는 메소드입니다.
-				$scope.chats.$add({
-					user: $rootScope.authData.facebook.displayName,
-					message: chat.message,
-					img: $rootScope.authData.facebook.profileImageURL
-				});
-				
-				//채팅내용을 전송하면 채팅창 비워줘야겠죠? 채팅메시지를 빈문자열로 초기화
-				chat.message = "";				
+
+				//페이스북으로 로그인한 경우
+				if($rootScope.authData.loginType == "facebook"){
+					$scope.chats.$add({
+						user: $rootScope.authData.facebook.displayName,
+						message: chat.message,
+						img: $rootScope.authData.facebook.profileImageURL
+					});
+					
+					//채팅내용을 전송하면 채팅창 비워줘야겠죠? 채팅메시지를 빈문자열로 초기화
+					chat.message = "";									
+				}
+
+				//이메일인증으로 로그인한 경우
+				else if($rootScope.authData.loginType == "email"){
+					$scope.chats.$add({
+						user: $rootScope.authData.password.email,
+						message: chat.message,
+						img: ""
+					});
+					
+					//채팅내용을 전송하면 채팅창 비워줘야겠죠? 채팅메시지를 빈문자열로 초기화
+					chat.message = "";
+				}
 			}
 			else{
 				alert("You need to login");
@@ -37,14 +53,47 @@ angular.module('starter.controllers', ['firebase'])
 
 
 .controller('AccountCtrl', function($scope, $rootScope) {
-	$scope.login = function(){
-		var ref = new Firebase("https://rhammer2.firebaseio.com");
+	//페이스북으로 로그인버전에서는 인증하는 메소드 내부에 선언했는데, 기능 확장을 위해서 바깥에 선언했습니다.
+	var ref = new Firebase("https://rhammer2.firebaseio.com");
+
+	$scope.login = function(username, password){
+		ref.authWithPassword({
+			email    : username,
+			password : password
+		}, function(error, authData) {
+			if (error) {
+				console.log("Login Failed!", error);
+			} else {
+				$rootScope.authData = authData;
+				$rootScope.authData.loginType = "email";
+				console.log("Authenticated successfully with payload:", authData);
+			}
+		});
+	};
+
+	$scope.register = function(username, password){
+		ref.createUser({
+			email    : username,
+			password : password
+		}, function(error, userData) {
+			if (error) {
+				console.log("Error creating user:", error);
+			} else {
+				$rootScope.authData = authData;
+				$rootScope.authData.loginType = "email";
+				console.log("Successfully created user account with uid:", userData.uid);
+			}
+		});
+	};
+
+	$scope.loginWithFacebook = function(){
 		ref.authWithOAuthPopup("facebook", function(error, authData) {
 		  if (error) {
 		    console.log("Login Failed!", error);
 		  } else {
 		    console.log("Authenticated successfully with payload:", authData);
 		    $rootScope.authData = authData;
+		    $rootScope.authData.loginType = "facebook"; //추가 인증수단이 생김으로서 facebook로그인임을 표시.
 		  }
 		},{remember:"default"});
 	}
