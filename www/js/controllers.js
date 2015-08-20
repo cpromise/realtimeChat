6092b33784c54152545dc69fe20e14dbea85248d
cpromise <cpromise1@naver.com>
2015-08-20 23:52:33 +0900

    keep logged in with localstorage
    -

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,8 +2,8 @@ angular.module('starter.controllers', ['firebase'])
 
 
 
-.controller('ChatsCtrl', ['$scope', '$firebase', '$rootScope',
-	function( $scope, $firebase, $rootScope){
+.controller('ChatsCtrl', ['$scope', '$firebase', '$rootScope', '$localstorage',
+	function( $scope, $firebase, $rootScope, $localstorage){
 		//firebase1탄에서 만들었던 본인의 계정을 입력해주세요
 		var ref = new Firebase('http://rhammer2.firebaseio.com/');
 
@@ -15,16 +15,15 @@ angular.module('starter.controllers', ['firebase'])
 
 		//채팅에서 전송을 하면 실행될 메소드
 		$scope.sendChat = function(chat){
-
-			if($rootScope.authData){
-				//$add는 Firebase에서 제공하는 메소드입니다.
+			if($localstorage.get("authData")){
+				var authData = $localstorage.getObject("authData");
 
 				//페이스북으로 로그인한 경우
-				if($rootScope.authData.loginType == "facebook"){
+				if(authData.loginType == "facebook"){
 					$scope.chats.$add({
-						user: $rootScope.authData.facebook.displayName,
+						user: authData.facebook.displayName,
 						message: chat.message,
-						img: $rootScope.authData.facebook.profileImageURL
+						img: authData.facebook.profileImageURL
 					});
 					
 					//채팅내용을 전송하면 채팅창 비워줘야겠죠? 채팅메시지를 빈문자열로 초기화
@@ -32,9 +31,9 @@ angular.module('starter.controllers', ['firebase'])
 				}
 
 				//이메일인증으로 로그인한 경우
-				else if($rootScope.authData.loginType == "email"){
+				else if(authData.loginType == "email"){
 					$scope.chats.$add({
-						user: $rootScope.authData.password.email,
+						user: authData.password.email,
 						message: chat.message,
 						img: ""
 					});
@@ -52,9 +51,10 @@ angular.module('starter.controllers', ['firebase'])
 
 
 
-.controller('AccountCtrl', function($scope, $rootScope) {
+.controller('AccountCtrl', function($scope, $rootScope, $localstorage) {
 	//페이스북으로 로그인버전에서는 인증하는 메소드 내부에 선언했는데, 기능 확장을 위해서 바깥에 선언했습니다.
 	var ref = new Firebase("https://rhammer2.firebaseio.com");
+	var isLogged = $localstorage.get("authData") != null;
 
 	$scope.login = function(username, password){
 		ref.authWithPassword({
@@ -64,8 +64,13 @@ angular.module('starter.controllers', ['firebase'])
 			if (error) {
 				console.log("Login Failed!", error);
 			} else {
-				$rootScope.authData = authData;
-				$rootScope.authData.loginType = "email";
+				// 기존에 사용하던 rootScope사용 코드를 삭제해줍니다.
+				// $rootScope.authData = authData;
+				// $rootScope.authData.loginType = "email";
+
+				// localstorage에 로그인정보를 담습니다.
+				authData.loginType = "email";
+				$localstorage.setObject("authData",authData);
 				console.log("Authenticated successfully with payload:", authData);
 			}
 		});
@@ -79,8 +84,7 @@ angular.module('starter.controllers', ['firebase'])
 			if (error) {
 				console.log("Error creating user:", error);
 			} else {
-				$rootScope.authData = authData;
-				$rootScope.authData.loginType = "email";
+				//register에서는 로그인이 되지 않도록 수정했습니다.
 				console.log("Successfully created user account with uid:", userData.uid);
 			}
 		});
@@ -117,8 +121,11 @@ angular.module('starter.controllers', ['firebase'])
 		    console.log("Login Failed!", error);
 		  } else {
 		    console.log("Authenticated successfully with payload:", authData);
-		    $rootScope.authData = authData;
-		    $rootScope.authData.loginType = "facebook"; //추가 인증수단이 생김으로서 facebook로그인임을 표시.
+		    // $rootScope.authData = authData;
+		    // $rootScope.authData.loginType = "facebook"; //추가 인증수단이 생김으로서 facebook로그인임을 표시.
+
+		    authData.loginType = "facebook"
+			$localstorage.setObject("authData",authData);
 		  }
 		},{remember:"default"});
 	}
